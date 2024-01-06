7896b132ce298869c8ff7a9ede9454d4b773c2e9
ffletcherr <homayouneemoradi@gmail.com>
2024-01-06 08:17:25 +0330

    train check

--- train.py
+++ train.py
@@ -2,5 +2,11 @@ from ultralytics.models.yolo.detect import DetectionTrainer
 
 
 trainer = DetectionTrainer(
-    overrides={"model": "yolov8n", "data": "./ultralytics/cfg/datasets/coco8.yaml"}
+    overrides={
+        "model": "yolov8n",
+        "epochs": 1,
+        "data": "./ultralytics/cfg/datasets/coco8.yaml",
+        "augment": False,
+    }
 )
+trainer.train()
